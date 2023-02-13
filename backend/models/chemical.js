13ba89ac09515a5c109d86ee8361cd939d091878
Khanh Duong T. B <84130912+KDuongThB@users.noreply.github.com>
2023-02-13 13:38:15 +0700

    update models

--- backend/models/chemical.js
+++ backend/models/chemical.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const chemicalSchema = new Schema()
+const chemicalSchema = new Schema({
+    chemicalName: {
+        type: String,
+        require: true
+    },
+    avoid: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chemical"
+    }]
+})
 
 module.exports = Chemical = mongoose.model("Chemical", chemicalSchema);
--- backend/models/product.js
+++ backend/models/product.js
@@ -1,6 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema()
+const productSchema = new Schema({
+    productName: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    age: {
+        type: Number,
+        require: true,
+    },
+    skin: {
+        type: String,
+    },
+    chemicals: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chemical",
+        require: true
+    }],
+    uses: [{
+        type: String
+    }],
+    instructions: [{
+        type: String
+    }],
+    limit: [{
+        type: Boolean
+    }],
+    stepType: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "StepType",
+        require: true
+    }
+})
 
 module.exports = Product = mongoose.model("Product", productSchema);
